1ac27870e1fb8cfe4b49b43b0ce028f5af8b7ce8
isabellechalhoub <isabelle.chalhoub@gmail.com>
2016-10-19 23:56:29 -0600

    imported crystal's scene to prototype project
    ready to get gameobjects imported into new scene and linked up.

---
+++ F L i C K E R crystal/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,252 @@
+﻿using UnityEngine;
+using System.Collections;
+using Prime31;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour 
+{
+#region Vars
+	public GameObject gameCamera;
+	public GameObject healthBar;
+	public GameObject gameOverPanel;
+	public GameObject winPanel;
+	public float walkSpeed = 3;
+	public float gravity = -35;
+	public float jumpHeight = 2;
+	public int health = 100;
+	public BoxCollider2D coll;
+	public BoxCollider2D enemy;
+	public GameObject healthNum;
+    public GameObject shield;
+    public GameObject sword;
+
+    private Vector3 swordStartPos;
+    private Vector3 swordEndPos;
+	private bool shieldin = false;
+	private bool floatin = false;
+	private bool playerControl = true;
+	private int currHealth = 0;
+	private CharacterController2D _controller;
+	private AnimationController2D _animator;
+    private bool swinging =false;
+    private bool pause = false;
+    #endregion
+
+    void Start ()
+    {
+        shield = GameObject.FindGameObjectWithTag("Shield");
+        sword = GameObject.FindGameObjectWithTag("Sword");
+		coll = GameObject.FindGameObjectWithTag("Player").GetComponent<BoxCollider2D> ();
+		enemy = GameObject.FindGameObjectWithTag ("Enemy").GetComponent<BoxCollider2D> ();
+		_controller = gameObject.GetComponent<CharacterController2D>();
+		_animator = gameObject.GetComponent<AnimationController2D>();
+
+		gameCamera.GetComponent<CameraFollow2D> ().startCameraFollow (this.gameObject);
+		winPanel.SetActive(false);
+		gameOverPanel.SetActive(false);
+		currHealth = health;
+	}
+
+	void Update ()
+    {
+		if (playerControl) 
+		{
+			Vector3 velocity = PlayerInput ();
+			_controller.move (velocity * Time.deltaTime);
+		}
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pause = !pause;
+            if (pause)
+                Time.timeScale = 0;
+            else
+                Time.timeScale = 1;
+        }
+	}
+		
+#region Movement
+	private Vector3 PlayerInput()
+	{
+		Vector3 velocity = _controller.velocity;
+		velocity.x = 0;
+
+		#region moving platform parenting
+		if (_controller.isGrounded && _controller.ground != null && _controller.ground.tag.Equals("MovingPlatform")) 
+		{
+			this.transform.parent = _controller.ground.transform;
+		}
+		else 
+		{
+			if (this.transform.parent != null)
+				this.transform.parent = null;
+		}
+		#endregion
+
+		#region running left/right
+		// Left arrow key
+		if (Input.GetAxis ("Horizontal") < 0 && !shieldin && !swinging)
+		{
+			velocity.x = -walkSpeed;
+			if (_controller.isGrounded) 
+			{
+				_animator.setAnimation ("Walk");
+				_animator.setFacing ("Left");
+			}
+		}
+
+		// Right arrow key
+		else if (Input.GetAxis ("Horizontal") > 0 && !shieldin && !swinging) 
+		{
+			velocity.x = walkSpeed;
+			if (_controller.isGrounded) 
+			{
+				_animator.setAnimation ("Walk");
+				_animator.setFacing ("Right");
+			}
+		}
+		#endregion
+
+		#region idle
+		//Idle
+		else 
+		{
+			if (_controller.isGrounded && currHealth != 0) 
+			{
+				_animator.setAnimation("Idle");
+			}
+		}
+		#endregion
+
+		#region Jump/Float
+		// Space bar - Jump
+		if (Input.GetKeyDown (KeyCode.Space) && !shieldin && _controller.isGrounded && !swinging) 
+		{
+			_animator.setAnimation("Jump");
+			velocity.y = Mathf.Sqrt (2f * jumpHeight * -gravity);
+		} 
+		else if ((Input.GetKeyDown (KeyCode.Space) && !_controller.isGrounded) || floatin) 
+		{
+			//_animator.setAnimation("Float");
+			velocity.y = -2;
+			floatin = true;
+		}
+		if (Input.GetKey(KeyCode.Space) && !_controller.isGrounded)
+		{
+			//_animator.setAnimation("Float2");
+		}
+		if (_controller.isGrounded || Input.GetKeyUp (KeyCode.Space))
+		{
+			if (!_controller.isGrounded)
+			{
+				_animator.setAnimation("Fall");
+			}
+			floatin = false;
+			gravity = -35;
+		}
+		#endregion
+
+		#region shield
+		//Shield up and down
+		if (Input.GetAxis("Fire1") > 0) {
+			shieldin = true;
+		} else
+			shieldin = false;
+
+        if (Input.GetKey(KeyCode.X) && !swinging)
+        {
+            shieldin = true;
+            shield.SetActive(true);
+        }
+        else
+        {
+            shieldin = false;
+            shield.SetActive(false);
+        }
+        #endregion
+
+        #region sword swing
+        // swing dat sword bb
+        if (Input.GetKey(KeyCode.C) && !shieldin)
+        {
+            swinging = true;
+            sword.SetActive(true);
+            //Transform pos = sword.GetComponent<Transform>();
+            //swordStartPos = pos.localPosition;
+            //Vector3 axis = new Vector3(pos.localPosition.x, pos.localPosition.y - pos.localPosition.sqrMagnitude, 0);
+            //pos.RotateAround(axis, axis, 20 * Time.deltaTime);
+        }
+        else
+        {
+            swinging = false;
+            sword.SetActive(false);
+        }
+        #endregion
+
+        // Change velocity.
+        velocity.y += gravity * Time.deltaTime;
+		return velocity;
+	}
+
+//	void OnCollisionEnter(Collision col)
+//	{
+//		if (GetComponent<Collider>().GetComponent<Collider>().name == "Enemy")
+//			PlayerDamage(5);
+//	}
+
+#endregion
+
+#region Damage/Death/Winning
+	// When the player collides with the death collider
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		//Debug.Log ("trigger");
+		if (col.tag == "KillZ")
+			PlayerFallDeath ();
+		else if (col.tag == "Damaging")
+			PlayerDamage (10);
+		else if (col.tag == "YouWin")
+			Winning ();
+		else if (col.tag == "Enemy" && (Input.GetKey (KeyCode.X) || Input.GetKey(KeyCode.C))) {}
+		else if(col.tag == "Enemy")
+			PlayerDamage (2);
+	}
+		
+	private void Winning()
+	{
+		playerControl = false;
+		//_animator.setAnimation("Idle");
+		winPanel.SetActive(true);
+	}
+
+	// Changes player health when damage is taken. checks for death
+	private void PlayerDamage(int damage)
+	{
+		currHealth -= damage;
+		float normHealth = (float)currHealth/(float)health;
+		GameObject.Find ("Health").GetComponent<Text> ().text = currHealth.ToString();
+		//healthBar.GetComponent<RectTransform>().sizeDelta = new Vector2(normHealth*256, 32);
+
+		if (currHealth <= 0)
+			PlayerDeath();
+	}
+
+	// Play death animation
+	private void PlayerDeath()
+	{
+		//_animator.setAnimation("Death");
+		playerControl = false;
+		gameOverPanel.SetActive(true);
+	}
+
+	// Stops the camera follow and reduces health
+	private void PlayerFallDeath()
+	{
+		currHealth = 0;
+		GameObject.Find ("Health").GetComponent<Text> ().text = currHealth.ToString();
+		//healthBar.GetComponent<RectTransform>().sizeDelta = new Vector2(0, 32);
+		gameCamera.GetComponent<CameraFollow2D>().stopCameraFollow();
+		gameOverPanel.SetActive(true);
+	}
+#endregion
+}
